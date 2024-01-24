3bcc225848c5516ff2d40988882c13c10adf4e35
wsh096 <wsh096@gmail.com>
2024-01-24 22:07:09 +0900

    feat : moodmeter entity 작업
    1. jpa 사용을 위한 build.gradle 수정
    2. baseentity 작성
    참고
      a.(https://eocoding.tistory.com/60)
    3. MoodMeterEntity작성
    참고
      a.(https://velog.io/@gowjr207/Entity-%EC%97%90-%EC%93%B0%EC%9D%B4%EB%8A%94-%EC%96%B4%EB%85%B8%ED%85%8C%EC%9D%B4%EC%85%98)
    4. erd 작업 중
    https://www.erdcloud.com/d/ZgBwc3PJgFhuheby4
    
    Todo
    enum 값 통합 또는 통합과 같은 형태 고민하기.
    9개의 감정 값 표현 방법 고민

--- src/main/java/com/backend/moodmeter/model/MoodMeterEntity.java
+++ src/main/java/com/backend/moodmeter/model/MoodMeterEntity.java
@@ -1,5 +1,21 @@
 package com.backend.moodmeter.model;
 
-public class MoodMeterEntity {
+import com.backend.global.domain.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MoodMeterEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
 }
