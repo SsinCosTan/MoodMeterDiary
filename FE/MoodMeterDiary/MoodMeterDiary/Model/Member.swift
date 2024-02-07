8098532a83500f8bce4db4518dfaa31dc6ad4ed0
mint3382 <mskim0926@naver.com>
2024-02-07 20:50:56 +0900

    feat: Model로 Member 구조체 구현

---
+++ FE/MoodMeterDiary/MoodMeterDiary/Model/Member.swift
@@ -0,0 +1,15 @@
+//
+//  Member.swift
+//  MoodMeterDiary
+//
+//  Created by minsong kim on 1/26/24.
+//
+
+import Foundation
+
+struct Member {
+    let id: String
+    let password: String
+    var name: String
+    var diaryCount: Int
+}
