501c797f1d2dcf078c57d1065e3ddf51576d57b2
mint3382 <mskim0926@naver.com>
2024-02-07 20:50:28 +0900

    feat: MainViewController에 CalendarView 구현

--- FE/MoodMeterDiary/MoodMeterDiary/Controller/MainViewController.swift
+++ FE/MoodMeterDiary/MoodMeterDiary/Controller/MainViewController.swift
@@ -8,13 +8,34 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    let calendarView: UICalendarView = {
+        let calendar = UICalendarView()
+        calendar.calendar = .current
+        calendar.locale = .current
+        calendar.fontDesign = .monospaced
+        calendar.translatesAutoresizingMaskIntoConstraints = false
+        
+        return calendar
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        navigationItem.title = "MOODMETER"
+        configureCalendarView()
         // Do any additional setup after loading the view.
     }
-
+    
+    func configureCalendarView() {
+        view.addSubview(calendarView)
+        
+        NSLayoutConstraint.activate([
+            calendarView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            calendarView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            calendarView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            calendarView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
 
 }
 
