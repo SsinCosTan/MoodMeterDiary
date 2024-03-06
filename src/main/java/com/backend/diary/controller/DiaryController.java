7d95e4bd4d9087b0db92a37c79fda05750634c23
DESKTOP-DB74U1N\user <wsh096@gmail.com>
2024-03-06 21:06:07 +0900

    feat : FEATURE_diary controller 업데이트/삭제 제작, list는 user 제작 후 제작 예정

--- src/main/java/com/backend/diary/controller/DiaryController.java
+++ src/main/java/com/backend/diary/controller/DiaryController.java
@@ -1,13 +1,17 @@
 package com.backend.diary.controller;
 
-import com.backend.diary.model.DiaryDto;
-import com.backend.diary.service.DiaryService;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.diary.model.DiaryDto;
+import com.backend.diary.service.DiaryService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController("/api/mood-meter")
 @RequiredArgsConstructor
@@ -16,14 +20,46 @@ public class DiaryController {
     private final DiaryService diaryService;
 
     /*
-    CRUD
-    Read : List 값 가져오기
+     * CRUD
+     * Read : List 값 가져오기
      */
+    // TODO UserId 를 받아 해당 권한의 사람에게 매핑되게 저장
     @PostMapping("/diaries")
-    public ResponseEntity<?> postDiary(@RequestBody DiaryDto diaryDto){
+    public ResponseEntity<?> postDiary(@RequestBody DiaryDto diaryDto) {
         String result = diaryService.setDiary(diaryDto);
-        if(!result.equals("success"))
+        if (!result.equals("success"))
             return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    // TODO getList
+    @Getmapping("/diaries")
+    public ResponseEntity<?> getDiary(@RequestParam long UserId) {
+
+        List<DiaryDto> result = diaryService.getDiaryList(bookId, diaryDto);
+        if (result.isEmpty())
+            return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    // TODO UserId 를 받아 적절한 권한을 가진 사람이 수정하는지 확인
+    @PutMapping("/diaries/{bookId}")
+    public ResponseEntity<?> putDiary(@PathVariable long bookId, @RequestBody DiaryDto diaryDto) {
+
+        String result = diaryService.updateDiary(bookId, diaryDto);
+        if (!result.equals("success"))
+            return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    // TODO UserId 를 받아 적절한 권한을 가진 사람이 수정하는지 확인
+    @DeleteMapping("/diaries/{bookId}")
+    public ResponseEntity<?> deleteDiary(@PathVariable long bookId, @RequestParam long UserId) {
+
+        String result = diaryService.deletedeleteDiary(bookId, UserId);
+        if (!result.equals("success"))
+            return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
--- src/main/java/com/backend/diary/service/DiaryServiceImpl.java
+++ src/main/java/com/backend/diary/service/DiaryServiceImpl.java
@@ -1,15 +1,15 @@
 package com.backend.diary.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
 import com.backend.diary.model.DiaryDto;
 import com.backend.diary.model.DiaryEntity;
 import com.backend.diary.model.DiaryRepository;
-import com.backend.moodmeter.config.MoodMeterColor;
-import com.backend.moodmeter.model.MoodMeterDto;
-import com.backend.moodmeter.model.MoodMeterEntity;
-import com.backend.moodmeter.model.MoodMeterRepository;
-import java.util.List;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 //Todo
 @Service
@@ -17,35 +17,68 @@ import org.springframework.stereotype.Service;
 public class DiaryServiceImpl implements DiaryService {
 
     private final DiaryRepository diaryRepository;
-    //CRUD
-    //Create
+    // CRUD
+    // Create
 
-    //Todo UserID, DiaryID와 같이 저장 되어야 함
+    // Todo UserID, DiaryID와 같이 저장 되어야 함
     @Override
     public String setDiary(DiaryDto diaryDto) {
-    DiaryEntity diary = DiaryEntity.builder()
-        .title(diaryDto.getTitle())
-        .description(diaryDto.getDescription())
-        .mood(diaryDto.getMood()).build();
+        DiaryEntity diary = DiaryEntity.builder()
+                .title(diaryDto.getTitle())
+                .description(diaryDto.getDescription())
+                .mood(diaryDto.getMood()).build();
         diaryRepository.save(diary);
         return "success";
     }
 
-    private MoodMeterEntity setMoodMeter(MoodMeterColor moodMeterColor) {
-
+    @Override
+    public String setDiary(DiaryDto diaryDto) {
+        DiaryEntity diary = DiaryEntity.builder()
+                .title(diaryDto.getTitle())
+                .description(diaryDto.getDescription())
+                .mood(diaryDto.getMood()).build();
+        diaryRepository.save(diary);
+        return "success";
     }
 
-    //Read
-    //UserId에 맞춰서 해당 월의 무드미터 정보를 가지고 와야 함.
     @Override
-    public List<MoodMeterDto> getMoodMeterList() {
+    public String updateDiary(long bookId, DiaryDto diaryDto) {
+        Optional<DiaryEntity> optionalDiary = diaryRepository.findById(bookId);
+        if (optionalDiary.isEmpty()) {
+            return "fail";
+        } else {
+            DiaryEntity diary = optionalDiary.get();
+            if (diaryDto.getTitle() != null) {
+                diary.setTitle(diaryDto.getTitle());
+            }
+            if (diaryDto.getDescription() != null) {
+                diary.setDescription(diaryDto.getDescription());
+            }
+            diaryRepository.save(diary);
+        }
+        return "success";
+    }
+
+    // Read
+    // UserId에 맞춰서 해당 월의 무드미터 정보를 가지고 와야 함.
+    // TODO Make user
+    @Override
+    public List<DiaryDto> getDiaryList(long UserId) {
+
         return null;
     }
 
-    //Delete 삭제_일기 삭제 시 함께 삭제. 복원 가능
+    // Delete 삭제_일기 삭제 시 함께 삭제. 복원 가능
     @Override
-    public void deleteMoodMeter(long bookId, long UserId) {
-        //Todo mapping BookId, userId DeletedAt
+    public String deletedeleteDiary(long bookId, long UserId) {
+        Optional<DiaryEntity> optionalDiary = diaryRepository.findById(bookId);
+        if (optionalDiary.isEmpty()) {
+            return "fail";
+        } else {
+            DiaryEntity diary = optionalDiary.get();
+            diary.setDeletedAt(true);
+            diaryRepository.save(diary);
+        }
+        return "success";
     }
-
 }
